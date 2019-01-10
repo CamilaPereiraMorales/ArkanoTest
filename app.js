662c8f623e9e77594c0db1aa69da20caa383aed0
Camili <camipereira2111@gmail.com>
2019-01-09 21:03:39 -0300

    maquetado ok

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 // Sidenav Initialization
 
-
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
-
-  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-  // var collapsibleElem = document.querySelector('.collapsible');
-  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
   // Or with jQuery
 
   $(document).ready(function(){
@@ -18,11 +7,7 @@
   });
 
   //date Initialization
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
-
+  
   // Or with jQuery
 
   $(document).ready(function(){
