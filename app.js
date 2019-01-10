84891465ef244a1859bce3b435c954de231bae8a
Camili <camipereira2111@gmail.com>
2019-01-09 21:46:08 -0300

    elimino campo fecha para guardarlo desde firebase

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 // Sidenav Initialization
 
-  // Or with jQuery
-
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
 
-  //date Initialization
-  
-  // Or with jQuery
 
-  $(document).ready(function(){
-    $('.datepicker').datepicker();
-  });
+
  
